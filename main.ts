9b85444fd1899cba5805684aa570c56ca1d3a3a5
cynthiarodrigu3z <156138173+cynthiarodrigu3z@users.noreply.github.com>
2024-02-28 15:16:30 -0600

    array and start of function

--- main.ts
+++ main.ts
@@ -227,6 +227,40 @@ function spawn2 () {
         . . . . . . . . . . . . . . . . 
         `, SpriteKind.Player)
     tiles.placeOnTile(p12, tiles.getTileLocation(6, 5))
+    tiles2 = [
+    tiles.getTileAtLocation(tiles.getTileLocation(1, 0)),
+    tiles.getTileAtLocation(tiles.getTileLocation(3, 0)),
+    tiles.getTileAtLocation(tiles.getTileLocation(5, 0)),
+    tiles.getTileAtLocation(tiles.getTileLocation(7, 0)),
+    tiles.getTileAtLocation(tiles.getTileLocation(0, 1)),
+    tiles.getTileAtLocation(tiles.getTileLocation(2, 1)),
+    tiles.getTileAtLocation(tiles.getTileLocation(4, 1)),
+    tiles.getTileAtLocation(tiles.getTileLocation(6, 1)),
+    tiles.getTileAtLocation(tiles.getTileLocation(1, 2)),
+    tiles.getTileAtLocation(tiles.getTileLocation(3, 2)),
+    tiles.getTileAtLocation(tiles.getTileLocation(5, 2)),
+    tiles.getTileAtLocation(tiles.getTileLocation(7, 2)),
+    tiles.getTileAtLocation(tiles.getTileLocation(0, 3)),
+    tiles.getTileAtLocation(tiles.getTileLocation(2, 3)),
+    tiles.getTileAtLocation(tiles.getTileLocation(4, 3)),
+    tiles.getTileAtLocation(tiles.getTileLocation(6, 3)),
+    tiles.getTileAtLocation(tiles.getTileLocation(1, 4)),
+    tiles.getTileAtLocation(tiles.getTileLocation(3, 4)),
+    tiles.getTileAtLocation(tiles.getTileLocation(5, 4)),
+    tiles.getTileAtLocation(tiles.getTileLocation(7, 4)),
+    tiles.getTileAtLocation(tiles.getTileLocation(0, 5)),
+    tiles.getTileAtLocation(tiles.getTileLocation(2, 5)),
+    tiles.getTileAtLocation(tiles.getTileLocation(4, 5)),
+    tiles.getTileAtLocation(tiles.getTileLocation(6, 5)),
+    tiles.getTileAtLocation(tiles.getTileLocation(1, 6)),
+    tiles.getTileAtLocation(tiles.getTileLocation(3, 6)),
+    tiles.getTileAtLocation(tiles.getTileLocation(5, 6)),
+    tiles.getTileAtLocation(tiles.getTileLocation(7, 6)),
+    tiles.getTileAtLocation(tiles.getTileLocation(0, 7)),
+    tiles.getTileAtLocation(tiles.getTileLocation(2, 7)),
+    tiles.getTileAtLocation(tiles.getTileLocation(4, 7)),
+    tiles.getTileAtLocation(tiles.getTileLocation(6, 7))
+    ]
 }
 function spawn1 () {
     t1 = sprites.create(img`
@@ -457,8 +491,45 @@ function spawn1 () {
         . . . . . . . . . . . . . . . . 
         `, SpriteKind.Player)
     tiles.placeOnTile(t12, tiles.getTileLocation(7, 2))
+    tiles2 = [
+    tiles.getTileAtLocation(tiles.getTileLocation(1, 0)),
+    tiles.getTileAtLocation(tiles.getTileLocation(3, 0)),
+    tiles.getTileAtLocation(tiles.getTileLocation(5, 0)),
+    tiles.getTileAtLocation(tiles.getTileLocation(7, 0)),
+    tiles.getTileAtLocation(tiles.getTileLocation(0, 1)),
+    tiles.getTileAtLocation(tiles.getTileLocation(2, 1)),
+    tiles.getTileAtLocation(tiles.getTileLocation(4, 1)),
+    tiles.getTileAtLocation(tiles.getTileLocation(6, 1)),
+    tiles.getTileAtLocation(tiles.getTileLocation(1, 2)),
+    tiles.getTileAtLocation(tiles.getTileLocation(3, 2)),
+    tiles.getTileAtLocation(tiles.getTileLocation(5, 2)),
+    tiles.getTileAtLocation(tiles.getTileLocation(7, 2)),
+    tiles.getTileAtLocation(tiles.getTileLocation(0, 3)),
+    tiles.getTileAtLocation(tiles.getTileLocation(2, 3)),
+    tiles.getTileAtLocation(tiles.getTileLocation(4, 3)),
+    tiles.getTileAtLocation(tiles.getTileLocation(6, 3)),
+    tiles.getTileAtLocation(tiles.getTileLocation(1, 4)),
+    tiles.getTileAtLocation(tiles.getTileLocation(3, 4)),
+    tiles.getTileAtLocation(tiles.getTileLocation(5, 4)),
+    tiles.getTileAtLocation(tiles.getTileLocation(7, 4)),
+    tiles.getTileAtLocation(tiles.getTileLocation(0, 5)),
+    tiles.getTileAtLocation(tiles.getTileLocation(2, 5)),
+    tiles.getTileAtLocation(tiles.getTileLocation(4, 5)),
+    tiles.getTileAtLocation(tiles.getTileLocation(6, 5)),
+    tiles.getTileAtLocation(tiles.getTileLocation(1, 6)),
+    tiles.getTileAtLocation(tiles.getTileLocation(3, 6)),
+    tiles.getTileAtLocation(tiles.getTileLocation(5, 6)),
+    tiles.getTileAtLocation(tiles.getTileLocation(7, 6)),
+    tiles.getTileAtLocation(tiles.getTileLocation(0, 7)),
+    tiles.getTileAtLocation(tiles.getTileLocation(2, 7)),
+    tiles.getTileAtLocation(tiles.getTileLocation(4, 7)),
+    tiles.getTileAtLocation(tiles.getTileLocation(6, 7))
+    ]
 }
-function checkermoving (piece1: Sprite, piece2: Sprite) {
+function highlight (list: any[]) {
+    for (let value of list) {
+    	
+    }
     if (true) {
     	
     } else {
@@ -482,6 +553,7 @@ let t4: Sprite = null
 let t3: Sprite = null
 let t2: Sprite = null
 let t1: Sprite = null
+let tiles2: Image[] = []
 let p12: Sprite = null
 let p11: Sprite = null
 let p10: Sprite = null
@@ -494,6 +566,12 @@ let p4: Sprite = null
 let p3: Sprite = null
 let p2: Sprite = null
 let p1: Sprite = null
+let list: number[] = []
+namespace userconfig{
+    export const ARCADE_SCREEN_WIDTH = 128
+    export const ARCADE_SCREEN_HEIGHT = 128
+}
 tiles.setCurrentTilemap(tilemap`level2`)
 spawn1()
 spawn2()
+highlight(list)
