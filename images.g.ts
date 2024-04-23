b141a30d8efd78cc4960e534d9ce32f314240af2
cynthiarodrigu3z <156138173+cynthiarodrigu3z@users.noreply.github.com>
2024-04-23 16:28:44 -0500

    set up sprites and function

--- images.g.ts
+++ images.g.ts
@@ -5,22 +5,59 @@ namespace myImages {
         switch(helpers.stringTrim(name)) {
             case "image1":
             case "highlight":return img`
-7 7 7 7 . . . . . . . . . . . . 
-7 . . . . . . . . . . . . . . . 
-7 . . . . . . . . . . . . . . . 
-7 . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . 7 7 . . . . . . . 
-. . . . . . . 7 7 . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . 7 
-. . . . . . . . . . . . . . . 7 
-. . . . . . . . . . . . . . . 7 
-. . . . . . . . . . . . 7 7 7 7 
+bbbb......cccccc
+c..............c
+c..............c
+c..............c
+c..............c
+c..............c
+c..............c
+c......77......c
+c......77......c
+c..............c
+c..............c
+c..............c
+c..............c
+c..............c
+c..............c
+c...........777c
+c..............c
+c..............c
+c..............c
+cccccccccccccccc
+`;
+            case "|_:#9o8;]XVp,Z+beR!u":
+            case "card":return img`
+...ccccccccccccccccccc...
+.cccbbbdddddddddddbbbcc..
+.cccbbbdddddddddddbbbcc..
+.ccbbbdddddddddddddbbbc..
+.ccbdddddddddddddddddbc..
+.ccbdddddddddddddddddbc..
+.ccdddddddddddddddddddc..
+.ccdddddddc6bbddddddddc..
+.ccdddddddc6bbddddddddc..
+.ccdddddccb666bdddddddc..
+.ccddddcbb66996bbdddddc..
+.ccddddcbb66996bbdddddc..
+.ccddddcbb66996bbdddddc..
+.ccdddd66669999666ddddc..
+.ccdddd66669999666ddddc..
+.ccddddcbb66996bbdddddc..
+.ccddddcbb66996bbdddddc..
+.ccddddcbb66996bbdddddc..
+.ccdddddccb666bdddddddc..
+.ccdddddccb666bdddddddc..
+.ccdddddddcb66ddddddddc..
+.ccdddddddddddddddddddc..
+.ccdddddddddddddddddddc..
+.ccdddddddddddddddddddc..
+.ccbdddddddddddddddddbc..
+.ccbdddddddddddddddddbc..
+.ccbbbdddddddddddddbbbc..
+.cccbbbdddddddddddbbbcc..
+.cccbbbdddddddddddbbbcc..
+...ccccccccccccccccccc...
 `;
         }
         return null;
